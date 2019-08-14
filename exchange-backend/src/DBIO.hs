79c14b77e06606e7d138b101f423a32b1bf8adc1
Vasilii Nechaev <Vasilii.Nechaev@capgroup.com>
2019-08-14 15:31:26 -0700

    ...

--- exchange-backend/src/DBIO.hs
+++ exchange-backend/src/DBIO.hs
@@ -11,19 +11,29 @@ module DBIO(
 ) where
 
 import Control.Monad
-import Control.Monad.IO.Class (liftIO)
+import Control.Monad.IO.Class
 import Data.ByteString (ByteString)
 import Data.Pool
 import Database.PostgreSQL.Simple
-import Servant (Handler)
 
 type ConnectionPool = Pool Connection
 type DBOp a = Connection -> IO a
 
-data DBIO a = Pure DBOp a | Wrapped IO (DBIO a)
+data DBIO a = Pure DBOp a | Wrapped (IO (DBIO a))
 
-instance Functor DBIO
-where fmap f dbio = Wrapped 
+instance MonadIO DBIO where
+  liftIO m = Pure $ const m
+
+instance Functor DBIO where
+  fmap fn (Pure dbop) = Pure dbop <$> fn
+  fmap fn Wrapped io = fn <$> io
+
+instance Applicative DBIO where
+  pure m = Pure $ const m
+--  (<*>) fn Pure dbop = Pure $ fn <$> 
+
+-- instance Monad DBIO where
+  
 
 runDBIO :: ConnectionPool -> DBIO a -> IO a
 runDBIO pool dbio = withResource pool $ \conn -> exec conn dbio
@@ -31,7 +41,7 @@ runDBIO pool dbio = withResource pool $ \conn -> exec conn dbio
         exec conn (Pure a) = a conn
         exec conn (Free fa) = fa >>= \a -> exec conn a
 
-liftDBIO :: ConnectionPool -> DBIO a -> Handler a
+liftDBIO :: MonadIO m => ConnectionPool -> DBIO a -> m a
 liftDBIO pool dbio = liftIO $ runDBIO pool dbio
 
 type ConnectionString = ByteString
@@ -44,8 +54,12 @@ initConnectionPool connStr =
              10 -- unused connections are kept open for a minute
              10 -- max. 10 connections open per stripe
 
+--flip3 :: (a -> b -> c -> d) -> b -> c -> a -> d
+--flip3 fn = flip fn
+
 queryListParam :: (ToRow q, FromRow r) => Query -> q -> DBOp [r]
 queryListParam qt q conn = Database.PostgreSQL.Simple.query conn qt q
 
 queryList :: (FromRow r) => Query -> DBOp [r]
 queryList qt conn = Database.PostgreSQL.Simple.query_ conn qt
+
